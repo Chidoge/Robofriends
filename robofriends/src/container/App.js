a0ecc254b81d64795d34cac87675b88a56ad74d5
Lincoln Choy <lincoln.choy1997@gmail.com>
2018-07-24 18:40:59 +1200

    Added searchbox which filters out card list according to searched name

--- robofriends/src/container/App.js
+++ robofriends/src/container/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 
 class App extends Component {
@@ -6,16 +7,29 @@ class App extends Component {
 	constructor(props) {
 
 		super();
-		this.state = {
 
+		this.state = {
+			robots : props.robots,
+			searchField : ''
 		}
 	}
 
+	onSearchChange = (event) => {
+		this.setState({searchField : event.target.value});
+	}
+
+
 	render() {
 
+		const filteredRobots = this.state.robots.filter(robot => {
+			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+		});
+
 		return (
-			<div className="App">
-				<CardList robots = {this.props.robots} />
+			<div className = 'tc'>
+				<h1>Robofriends</h1>
+				<SearchBox onSearchChange = {this.onSearchChange} />
+				<CardList robots = {filteredRobots} />
 			</div>
 		);
 
